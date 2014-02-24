abb8fd7d883983366558f312794c199cd6edad26
Enrique Delgado <delgado.enrique@gmail.com>
2014-02-24 00:35:52 -0600

    Added read benchmark.

--- benchmark.rb
+++ benchmark.rb
@@ -1,9 +1,10 @@
 require 'benchmark'
 require 'net/http'
 require 'uri'
+require 'json'
 require 'pry'
 
-numtimes = 10
+numtimes = 1000
 url = "http://0.0.0.0:9292/orders"
 
 total = 0
@@ -11,16 +12,32 @@ puts url
 puts '=' * url.length
 uri = URI.parse(url)
 http = Net::HTTP.new(uri.host, uri.port)
+uuids = []
 
 (1..numtimes.to_i).each do |n|
+  response = nil
   time = Benchmark.realtime do
     request = Net::HTTP::Post.new(uri.request_uri)
     request.set_form_data({"item_name" => "Item #{n}", "quantity" => n, "price" => n * 10})
+    response = http.request(request)
+  end  
+  puts "#{url} - #{n} : #{time}s"
+  total += time
+  order = JSON.parse(response.body)
+  uuids.push(order['uuid'])
+end
+puts '=' * url.length
+puts "Average for #{url} : #{(total / numtimes.to_i).round(4)}s or #{1 / (total / numtimes.to_i).round(6)} reqs/sec."
+puts "\n"
+
+(uuids).each do |n|
+  time = Benchmark.realtime do
+    request = Net::HTTP::Get.new(uri.request_uri)
     http.request(request)
   end
   puts "#{url} - #{n} : #{time}s"
   total += time
 end
 puts '=' * url.length
-puts "Average for #{url} : #{(total / numtimes.to_i).round(4)}s"
+puts "Average for #{url} : #{(total / numtimes.to_i).round(4)}s or #{1 / (total / numtimes.to_i).round(6)} reqs/sec."
 puts "\n"
