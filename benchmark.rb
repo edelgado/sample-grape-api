2dae745ffa432d0f3b911f2fe9ab070bc411be00
Enrique Delgado <delgado.enrique@gmail.com>
2014-02-27 17:45:40 -0600

    Trying without Hydra, as it seems to slow things down if there is only one worker on the API side.

--- benchmark.rb
+++ benchmark.rb
@@ -3,50 +3,34 @@ require 'json'
 require 'typhoeus'
 require 'pry'
 
-numtimes = 10 # How many reads and writes?
+numtimes = 30 # How many reads and writes?
+url = "http://0.0.0.0:9292/orders"
 
 # =================== POST Benchmark ====================
-hydra = Typhoeus::Hydra.hydra # A hundred serpents is better than one
 total = 0 # Total time (updates after each request)
-url = "http://0.0.0.0:9292/orders"
 uuids = [] # UUIDs of newly created records
 puts '=' * url.length
 puts url
 
 (1..numtimes.to_i).each do |n|
-
   # Compose the request
   request = Typhoeus::Request.new(
     url,
     method: :post,
     body: {"item_name" => "Item #{n}", "quantity" => n, "price" => n * 10}
   )
-
-  # Request callbacks
-  request.on_complete do |response|
-    if response.success?
-      # hell yeah
-        puts "POST #{url} - #{n}: #{(response.total_time - total).round(4)}s"
-        total = response.total_time
-        order = JSON.parse(response.body)
-        uuids.push(order['uuid'])
-    elsif response.timed_out?
-      # aw hell no
-      puts "Got a time out"
-    elsif response.code == 0
-      # Could not get an http response, something's wrong.
-      puts "Could not get an HTTP response, message was: #{response.return_message}"
-    else
-      # Received a non-successful http response.
-      puts "HTTP request failed: #{response.code.to_s}"
-    end
+  # Run it
+  request.run
+  # Get the response
+  response = request.response
+  # If successful, do a couple of things:
+  if response.success?
+    puts "POST #{url} - #{n}: #{(response.total_time).round(4)}s"
+    total += response.total_time
+    order = JSON.parse(response.body)
+    uuids.push(order['uuid'])
   end
-  # Queue the request
-  hydra.queue(request);
-  # binding.pry
 end
-
-hydra.run # Release the Hydra!
 
 # binding.pry
 puts '=' * url.length
@@ -55,41 +39,26 @@ puts "\n"
 
 
 # =================== GET Benchmark ====================
-hydra = Typhoeus::Hydra.hydra # A hundred serpents is better than one
 total = 0 # Reset the total time counter
 
+# Lets read back the UUIDs we just wrote:
 (uuids).each do |n|
-
   # Compose the request
   request = Typhoeus::Request.new(
     url + '/' + n,
     method: :get
   )
-
-  # Request callbacks
-  request.on_complete do |response|
-    if response.success?
-      # hell yeah
-        order = JSON.parse(response.body)
-        puts "GET #{url} - #{order['uuid']}: #{(response.total_time - total).round(4)}s"
-        total = response.total_time
-    elsif response.timed_out?
-      # aw hell no
-      puts "Got a time out"
-    elsif response.code == 0
-      # Could not get an http response, something's wrong.
-      puts "Could not get an HTTP response, message was: #{response.return_message}"
-    else
-      # Received a non-successful http response.
-      puts "HTTP request failed: #{response.code.to_s}"
-    end
+  # Run it
+  request.run
+  # Get the response
+  response = request.response
+  # If successful, do a couple of things:
+  if response.success?
+    order = JSON.parse(response.body)
+    puts "GET #{url} - #{order['uuid']}: #{(response.total_time).round(4)}s"
+    total += response.total_time
   end
-
-  # Queue the request
-  hydra.queue(request);
 end
-
-hydra.run # Release the Hydra!
 
 puts '=' * url.length
 puts "Average for #{url} : #{(total / numtimes.to_i).round(4)}s or #{1 / (total / numtimes.to_i).round(4)} reqs/sec."
